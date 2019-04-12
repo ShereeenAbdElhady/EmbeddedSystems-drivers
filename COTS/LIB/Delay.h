7a74d4a70afba6c3f285e3e5077b5042b774eb23
ShereeenAbdElhady <47639736+ShereeenAbdElhady@users.noreply.github.com>
2019-04-12 23:03:37 +0200

    Add files via upload

---
+++ COTS/LIB/Delay.h
@@ -0,0 +1,15 @@
+/*
+ * File Name: Delay.h
+ * Description: header file contains delay functions' prototypes
+ *  Created on: Jan 25, 2019
+ *  Author: shereen
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+extern void delay_milliSecond(u32 value);
+extern void delay_microSecond(u32 value);
+
+
+#endif /* DELAY_H_ */
